ce554e767f49aedd5038b0c2e57231e9ccc67fbb
Tomasz Zaleski <tomek30000@gmail.com>
2018-12-29 01:23:16 +0100

    Added logging

--- src/main/java/agh/cs/POprojekt/Main.java
+++ src/main/java/agh/cs/POprojekt/Main.java
@@ -7,9 +7,8 @@ import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,15 +32,21 @@ public class Main {
             PrintWriter writer = terminal.writer();
 
             File file;
-            if(args.length == 0) file = new File(reader.readLine(System.getProperty("user.dir") + "> "));
+            if(args.length < 1) file = new File(reader.readLine("Input folder: " + System.getProperty("user.dir") + "/"));
             else file = new File(args[0]);
 
             while (!file.isDirectory()) {
                 writer.println("Invalid directory");
-                file = new File(reader.readLine(System.getProperty("user.dir") + "> "));
+                file = new File(reader.readLine("Input folder: " + System.getProperty("user.dir") + "> "));
             }
 
             List<Judgment> judgments = new Loader().load(file);
+
+            File logFile;
+            if (args.length < 2) logFile = new File(reader.readLine("Save file location: " + System.getProperty("user.dir") + "/"));
+            else logFile = new File(args[1]);
+
+            PrintWriter logWriter = new PrintWriter(new BufferedWriter(new FileWriter(logFile)));
 
             while (true) {
                 String[] commandStr = reader.readLine("command> ").split("\\s", 2);
@@ -50,8 +55,16 @@ public class Main {
                     writer.println("Unknown command, type 'help' for help");
                     continue;
                 }
-                if (commandStr.length < 2) writer.println(command.run("", judgments));
-                else writer.println(command.run(commandStr[1], judgments));
+
+                String result;
+                if (commandStr.length < 2) result = command.run("", judgments);
+                else result = command.run(commandStr[1], judgments);
+                writer.println(result);
+                logWriter.print("command> " + commandStr[0]);
+                if (commandStr.length > 1) logWriter.print(" " + commandStr[1]);
+                logWriter.println();
+                logWriter.println(result);
+                logWriter.flush();
             }
 //            System.out.println(commandMap.get("help").run("", judgments));
         } catch (IOException e) {
